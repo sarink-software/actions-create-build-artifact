445ae3cafd8edf92fab9750f66ca269b06898311
Kabir Sarin <sarink87@gmail.com>
2020-12-16 14:23:26 -0600

    Compress artifact

--- index.ts
+++ index.ts
@@ -26,7 +26,7 @@ const asyncExec = promisify(origExec);
     const exclude = [...core.getInput('exclude').split(' '), artifactFileName].map(
       (file) => `--exclude=${file}`
     );
-    await exec(`tar -cvf ${artifactFileName} ${exclude.join(' ')} ${include.join(' ')}`);
+    await exec(`tar -czvf ${artifactFileName} ${exclude.join(' ')} ${include.join(' ')}`);
 
     const artifactClient = artifact.create();
     await artifactClient.uploadArtifact(artifactFileName, [`./${artifactFileName}`], './');
