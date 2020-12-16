9d6fefc21d3ddfbfc69e7c2bb6fb0768542b0ae7
Kabir Sarin <sarink87@gmail.com>
2020-12-16 01:36:43 -0600

    Rewrite with promisified exec, include/exclude inputs, and updated tests

--- index.ts
+++ index.ts
@@ -1,57 +1,37 @@
 // Import modules with "* as" https://github.com/vercel/ncc/issues/621
 import * as core from '@actions/core';
 import * as artifact from '@actions/artifact';
-import * as childProcess from 'child_process';
 import * as shortuuid from 'short-uuid';
+import { promisify } from 'util';
+import { exec as origExec } from 'child_process';
+const asyncExec = promisify(origExec);
 
 (async () => {
   try {
-    const exec = (command: string, args: string[] = []) => {
-      return new Promise((resolve) => {
-        const { spawn } = childProcess;
-
-        const PS1 = 'actions$';
-        core.info(`${PS1} ${command} ${args.join(' ')}`);
-
-        const proc = spawn(command, args);
-
-        const stdout: string[] = [];
-        proc.stdout.on('data', (data) => {
-          stdout.push(data);
-          core.info(data);
-        });
-
-        const stderr: string[] = [];
-        proc.stderr.on('data', (data) => {
-          stderr.push(data);
-          throw new Error(stderr.join('\n'));
-        });
-
-        proc.on('close', (code) => {
-          if (code !== 0) throw new Error(stderr.join('\n'));
-          return resolve(stdout.join('\n'));
-        });
-      });
+    const exec = async (command: string) => {
+      const PS1 = 'actions$';
+      core.info(`${PS1} ${command}`);
+      const { stdout, stderr } = await asyncExec(command);
+      core.info(stdout);
+      if (stderr) throw new Error(stderr);
     };
 
     const buildCommand = core.getInput('build-command', { required: true });
-    await exec('bash', ['-cxe', buildCommand]);
+    await exec(buildCommand);
 
-    const artifactFileName = `${shortuuid.generate()}.tar.gz`;
-    const exclude = [
-      ...core
-        .getInput('exclude-files')
-        .split(' ')
-        .map((file) => `--exclude=${file}`),
-      `--exclude=${artifactFileName}`,
-    ];
-    const keep = core.getInput('keep-files').split(' ');
-    await exec('tar', ['-cvzf', artifactFileName, ...exclude, ...keep]);
+    const artifactFileName = `${shortuuid.generate()}.tar`;
+    await exec(`touch ${artifactFileName}`);
+
+    const include = core.getInput('include').split(' ');
+    const exclude = [...core.getInput('exclude').split(' '), artifactFileName].map(
+      (file) => `--exclude=${file}`
+    );
+    await exec(`tar -cvf ${artifactFileName} ${exclude.join(' ')} ${include.join(' ')}`);
 
     const artifactClient = artifact.create();
-    await artifactClient.uploadArtifact(artifactFileName, [artifactFileName], '.');
+    await artifactClient.uploadArtifact(artifactFileName, [`./${artifactFileName}`], './');
 
-    await exec('rm', ['-f', artifactFileName]);
+    await exec(`rm -f ${artifactFileName}`);
 
     core.setOutput('ARTIFACT_NAME', artifactFileName);
   } catch (error) {
