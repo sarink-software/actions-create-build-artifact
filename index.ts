bfb23fb41c1fd682d1c2db987bc449dd024c8b7b
Kabir Sarin <sarink87@gmail.com>
2021-10-19 00:25:34 -0500

    Fix silly try/catch to handle errors

--- index.ts
+++ index.ts
@@ -7,37 +7,34 @@ import { exec as origExec } from 'child_process';
 const asyncExec = promisify(origExec);
 
 (async () => {
-  try {
-    const exec = async (command: string) => {
-      const PS1 = 'actions$';
-      core.info(`${PS1} ${command}`);
-      const { stdout, stderr } = await asyncExec(command);
-      core.info(stdout);
-      if (stderr) throw new Error(stderr);
-    };
+  const exec = async (command: string) => {
+    const PS1 = 'actions$';
+    core.info(`${PS1} ${command}`);
+    const { stdout, stderr } = await asyncExec(command);
+    core.info(stdout);
+    if (stderr) throw new Error(stderr);
+  };
 
-    const buildCommand = core.getInput('build-command', { required: true });
-    await exec(buildCommand);
+  const buildCommand = core.getInput('build-command', { required: true });
+  await exec(buildCommand);
 
-    const artifactFileName = `${shortuuid.generate()}.tar`;
-    await exec(`touch ${artifactFileName}`);
+  const artifactFileName = `${shortuuid.generate()}.tar`;
+  await exec(`touch ${artifactFileName}`);
 
-    const include = core.getInput('include').split(' ');
-    const exclude = [...core.getInput('exclude').split(' '), artifactFileName].map(
-      (file) => `--exclude=${file}`
-    );
-    await exec(`tar -czvf ${artifactFileName} ${exclude.join(' ')} ${include.join(' ')}`);
+  const include = core.getInput('include').split(' ');
+  const exclude = [...core.getInput('exclude').split(' '), artifactFileName].map(
+    (file) => `--exclude=${file}`
+  );
+  await exec(`tar -czvf ${artifactFileName} ${exclude.join(' ')} ${include.join(' ')}`);
 
-    const artifactClient = artifact.create();
-    await artifactClient.uploadArtifact(artifactFileName, [`./${artifactFileName}`], './', {
-      retentionDays: 1,
-    });
+  const artifactClient = artifact.create();
+  await artifactClient.uploadArtifact(artifactFileName, [`./${artifactFileName}`], './', {
+    retentionDays: 1,
+  });
 
-    await exec(`rm -f ${artifactFileName}`);
+  await exec(`rm -f ${artifactFileName}`);
 
-    core.setOutput('ARTIFACT_NAME', artifactFileName);
-  } catch (error) {
-    core.setFailed(error.message);
-    throw error;
-  }
-})();
+  core.setOutput('ARTIFACT_NAME', artifactFileName);
+})().catch((error) => {
+  core.setFailed(error.message);
+});
